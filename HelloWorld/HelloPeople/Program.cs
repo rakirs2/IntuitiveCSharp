8a86b40e37be53e15b3770b62d0ff44519618b72
rakirs2 <srmylavarapu@microsoft.com>
2023-08-27 11:01:52 -0500

    adding hello world

---
+++ HelloWorld/HelloPeople/Program.cs
@@ -0,0 +1,12 @@
+﻿// create a new list of type string
+// variable name names
+// assign it default values
+var names = new List<string>() { "Bin", "Srikar", "Dandre" };
+
+// foreach
+// for every value in names, take the value of "name" in order and run it through the loop
+foreach (string name in names)
+{
+    Console.WriteLine($"Hello, {name}");
+}
+
