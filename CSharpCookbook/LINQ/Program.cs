538e48aef8e2be7a8d00894fc0c765b6d526064d
rakirs2 <srmylavarapu@microsoft.com>
2023-11-16 08:31:18 -0600

    adding to cookbook

---
+++ CSharpCookbook/LINQ/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] numbers = { 9, 34, 65, 92, 87, 435, 3, 54,
+    83, 23, 87, 435, 67, 12, 19 };
+
+int first = numbers.First(number => number > 80);
+int fail = numbers.FirstOrDefault(number => number > 1000, 300);
+Console.WriteLine(first);
+Console.WriteLine(fail);
