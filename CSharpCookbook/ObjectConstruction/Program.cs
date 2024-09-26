d16e494ce42477d15d1738484007769630876a7a
Srikar Mylavarapu <srikar.mylavarapu@gmail.com>
2024-09-26 11:58:43 -0500

    object construction

---
+++ CSharpCookbook/ObjectConstruction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+SomeObject obj = new();
+var someObject2 = (SomeObject)obj.Clone();
+
+public class SomeObject : ICloneable
+{
+    public SomeObject()
+    {
+    }
+
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
+}
