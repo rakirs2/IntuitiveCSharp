89ac2742ca12343762de44b8fdd85d9f406354be
Srikar Mylavarapu <srikar.mylavarapu@gmail.com>
2024-10-24 12:40:30 -0500

    Timespan

---
+++ CSharpCookbook/TimeSpan/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(new TimeSpan(60000));
+Console.WriteLine(TimeSpan.FromMinutes(1));
