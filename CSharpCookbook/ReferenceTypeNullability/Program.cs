bae0bd165ea7bdf59552589d0e650f6ac2e574a1
Srikar Mylavarapu <srmylavarapu+odspmdb@microsoft.com>
2024-12-04 07:17:49 -0600

    adding reference type nullability

---
+++ CSharpCookbook/ReferenceTypeNullability/Program.cs
@@ -0,0 +1,15 @@
+﻿static void SomeMethod(ref SomeClass someClass)
+{
+    someClass = null;
+}
+
+var thing = new SomeClass();
+
+
+SomeMethod(ref thing);
+
+Console.WriteLine(thing);
+
+class SomeClass{
+}
+
