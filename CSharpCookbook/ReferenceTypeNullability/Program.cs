5c90adeac324a70b46be3f231cae44997f2a6acb
Srikar Mylavarapu <srmylavarapu+odspmdb@microsoft.com>
2024-12-04 08:06:15 -0600

    updates

--- CSharpCookbook/ReferenceTypeNullability/Program.cs
+++ CSharpCookbook/ReferenceTypeNullability/Program.cs
@@ -3,12 +3,24 @@
     someClass = null;
 }
 
+
+static void SomeNullableMethod(SomeClass? someClass)
+{
+    someClass = null;
+}
+
 var thing = new SomeClass();
 
 
 SomeMethod(ref thing);
 
 Console.WriteLine(thing);
+
+var thing2 = new SomeClass();
+
+SomeNullableMethod(thing2);
+
+Console.WriteLine(thing2);
 
 class SomeClass{
 }
