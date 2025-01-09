8804e0797bdd5d081f9a8b521423e180c2bea598
Srikar Mylavarapu <srmylavarapu+odspmdb@microsoft.com>
2025-01-09 10:19:20 -0600

    updating, adding string compariosn

---
+++ CSharpCookbook/StringComparison/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+var lowerCase = "hello world";
+var upperCase = "HELLO WORLD";
+Console.WriteLine(lowerCase.Equals(upperCase, StringComparison.Ordinal));
+Assert.IsTrue(lowerCase.Equals(upperCase, StringComparison.OrdinalIgnoreCase));
+Assert.IsFalse(lowerCase.Equals(upperCase, StringComparison.Ordinal));
