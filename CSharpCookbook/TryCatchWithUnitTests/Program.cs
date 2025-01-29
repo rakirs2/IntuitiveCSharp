0b16236ccd9296adab798d67e0164dc9e8d0c0be
Srikar Mylavarapu <srmylavarapu+odspmdb@microsoft.com>
2025-01-29 15:24:01 -0600

    updating

---
+++ CSharpCookbook/TryCatchWithUnitTests/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+Console.WriteLine("Hello, World!");
+
+
+try
+{
+    Console.WriteLine("something");
+    Assert.Fail("unreachable");
+}
+//catch
+//{
+//    Console.WriteLine("Exception");
+//}
+finally
+{
+    Console.WriteLine("do we hit this");
+}
