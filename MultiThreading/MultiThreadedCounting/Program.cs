72e86789d412fc731264e157f4037e5b3a6e91fe
Srikar Mylavarapu <srikarmylavarapu@gmail.com>
2025-03-01 11:34:29 -0600

    Salim was right on round 1

---
+++ MultiThreading/MultiThreadedCounting/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Random random = new Random();
+// int32.max is less than what I'm going for here;
+int totalNumbers = 1000000;
+
+long runningTotal = 0;
+long output = 0;
+var allValues = new int[totalNumbers];
+for (int i = 0; i < totalNumbers; i++)
+{
+    var number = random.Next(0, 100);
+    allValues[i]= number;
+    runningTotal += number;
+}
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+foreach (var value in allValues)
+{
+    output += value;
+}
+stopwatch.Stop();
+
+Console.WriteLine($"Time Elapsed: {stopwatch.ElapsedTicks}, totalExpected: {runningTotal}, actual: {output}");
+
+// Apparently there's a LINQ for this
+stopwatch.Restart();
+int totalOfArrayLinq = allValues
+    .AsParallel()  // comment this out if you want sequential version
+    .Sum();
+stopwatch.Stop();
+Console.WriteLine($"Time Elapsed: {stopwatch.ElapsedTicks}, totalExpected: {runningTotal}, actual: {totalOfArrayLinq}");
